33318228cfa28476662396699c65345b2e220598
TaeBbong <mok05289@korea.ac.kr>
2025-05-04 22:06:44 +0900

    250504: 줄 서는 방법

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
-def solution(board):
-    start = find_start(board)
-    goal = find_goal(board)
-    answer = bfs(board, start, goal)
+def factorial(n):
+    r = 1
+    for i in range(1, n + 1):
+        r *= i
+    return r
+
+
+def solution(n, k):
+    answer = []
+    numbers = [(i + 1) for i in range(n)]
+    k -= 1
+    
+    for i in range(n - 1, 0, -1): # (n - 1)! ~ 2!
+        idc = factorial(i)
+        idx = k // idc
+        k %= idc
+        answer.append(numbers.pop(idx))
+    answer.append(numbers[0])
+    
     return answer
+        
 
 
 if __name__ == "__main__":
-    print(solution(["...D..R", ".D.G...", "....D.D", "D....D.", "..D...."]))
-    print(solution([".D.R", "....", ".G..", "...D"]))
+    print(solution(3, 5))
+    # print(solution())
