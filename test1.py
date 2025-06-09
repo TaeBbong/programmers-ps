a91c062d8d38172027fd35767210760155d0b01c
TaeBbong <mok05289@korea.ac.kr>
2025-06-09 18:29:51 +0900

    250609: 도넛과 막대 그래프*

--- test1.py
+++ test1.py
@@ -1,33 +1,26 @@
-def solution(info, n, m):
-    '''
-    dp[i][k] = min(dp[i - 1][k - info[i][1]], dp[i - 1][k] + info[i][0])
-    '''
-    INF = n + 1
-    answer = 0
-    l = len(info)
-    dp = [[INF for j in range(m)] for i in range(l)]
-    
-    for k in range(m):
-        if k < info[0][1]:
-            dp[0][k] = info[0][0]
-        else:
-            dp[0][k] = 0
-    
-    for i in range(1, l):
-        for k in range(m):
-            if k >= info[i][1]:
-                dp[i][k] = min(dp[i - 1][k - info[i][1]], dp[i - 1][k] + info[i][0])
-            else:
-                dp[i][k] = dp[i - 1][k] + info[i][0]
-    
-    answer = min(dp[l - 1])
-    if answer >= n:
-        return -1
-    return answer
+from collections import defaultdict
 
 
-if __name__ == "__main__":
-    solution([[1, 2], [2, 3], [2, 1]]	, 4, 4)
-    solution([[1, 2], [2, 3], [2, 1]]	, 1, 7)
-    solution([[3, 3], [3, 3]]	, 7, 1)
-    solution([[3, 3], [3, 3]]	, 6, 1)
+def solution(edges):
+    indeg  = defaultdict(int)
+    outdeg = defaultdict(int)
+
+    # 1. 각 노드의 in-degree, out-degree 계산
+    for s, e in edges:
+        outdeg[s] += 1
+        indeg[e]  += 1
+        # 존재만 보장
+        indeg.setdefault(s, 0)
+        outdeg.setdefault(e, 0)
+
+    # 2. 생성한 정점(센터) 찾기 : in = 0 & out >= 2
+    center = next(v for v in outdeg if indeg[v] == 0 and outdeg[v] >= 2)
+
+    # 3. 막대·8자 카운트 (센터는 제외)
+    stick = sum(1 for v in outdeg if v != center and outdeg[v] == 0)
+    eight = sum(1 for v in outdeg if v != center and outdeg[v] == 2)
+
+    # 4. 도넛 개수 = 센터 out-degree − 막대 − 8자
+    donut = outdeg[center] - stick - eight
+
+    return [center, donut, stick, eight]
